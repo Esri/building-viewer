10ee8974f497bab0d1cc7127cdda49c9ac9e2b24
Yannik Messerli <yannik.messerli@gmail.com>
2019-07-03 10:39:08 +0200

    Make portal an optional config

--- src/js/support/appUtils.ts
+++ src/js/support/appUtils.ts
@@ -2,20 +2,29 @@ import WebScene = require("esri/WebScene");
 import SceneView = require("esri/views/SceneView");
 import Layer = require("esri/layers/Layer");
 import Collection = require("esri/core/Collection");
+import config = require("../config");
+import PortalItem = require("esri/portal/PortalItem");
+import Portal = require("esri/portal/Portal");
 
 export function createViewFromWebScene(args: {
   mapContainer: string, 
   websceneId: string
 }) {
 
+  const portalItem = new PortalItem({
+    id: args.websceneId
+  });
+
+  // Let user add portal parameter
+  if (config.portalUrl) {
+    portalItem.portal = new Portal({
+      url: config.portalUrl
+    });
+  }
+
   // Load webscene and display it in a SceneView
   const webscene = new WebScene({
-     portalItem: {
-      id: args.websceneId,
-      portal: {
-        url: "https://zrh.mapsdevext.arcgis.com"
-      }
-    }
+    portalItem
   });
 
   const view = new SceneView({
@@ -23,20 +32,7 @@ export function createViewFromWebScene(args: {
     map: webscene
   });
 
-  // webscene.when(function() {
-  //   webscene.basemap = null;
-  //   webscene.ground.surfaceColor = backgroundColor;
-  // });
-
   view.when(() => {
-    // view.environment.lighting.directShadowsEnabled = true;
-    // view.environment.lighting.ambientOcclusionEnabled = true;
-    // view.environment.starsEnabled = false;
-    // (view.environment.background as any) = {
-    //   type: "color",
-    //   color: [0,0,0,0] as any
-    // };
-    // view.map.ground.surfaceColor =  [0,0,0,0] as any;
     view.padding = { left: 300 };
     view.popup.autoOpenEnabled = false;
   });
